9912f450437a1f0cfc967df055c7e44d07a30887
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2025-06-09 21:19:17 -0300

    Fix: Actions error (no sandbox)

--- auto-pdf/main.js
+++ auto-pdf/main.js
@@ -7,7 +7,9 @@ const header = `<h1 style=font-size:15px;text-align:center;width:100%>Currículo
 const footer = `<div style="font-size:10px;width:100%;padding:0 30px;display:flex;justify-content:space-between"><span style=display:inline;font-size:larger><small>PDF feito com pura programação.  <a href="https://github.com/AdrianoLMRS/AdrianoLMRS/tree/main/auto-pdf#readme">Veja mais</a></small></span><div style=margin-right:50px><a href=https://raw.githubusercontent.com/AdrianoLMRS/AdrianoLMRS/refs/heads/main/curriculo.pdf target=_blank style=margin-right:50px>Arquivo Bruto</a> <a href=https://github.com/AdrianoLMRS/AdrianoLMRS/blob/main/curriculo.pdf target=_blank>Ver no Github</a></div></div>`
 
 async function run() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] // Need this because Github Actions
+    });
     const page = await browser.newPage();
     await page.goto(`file://${process.cwd()}/src/curriculo.html`, { waitUntil: 'networkidle0' }); // Load the HTML content into the page and wait until there are no more network connections for at least 500 ms
     
